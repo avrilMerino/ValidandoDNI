853ebc7606e8e4410f974948d1bd0f4b2b7d12ed
avrilMerino <merinomorenoavri@gmail.com>
2025-10-15 13:35:33 +0200

    Prueba de commit

--- ValidandoDNI/Form1.cs
+++ ValidandoDNI/Form1.cs
@@ -19,7 +19,7 @@ namespace ValidandoDNI
 
         private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
-
+             
         }
 
         private void label1_Click(object sender, EventArgs e)
