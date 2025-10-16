7ceb27c0c8fc6da946e6ec6c43bdb68a9a095b67
avrilMerino <merinomorenoavril@gmail.com>
2025-10-16 10:03:46 +0200

    Lógica para que un DNI sea válido completada

--- ValidandoDNI/Form1.Designer.cs
+++ ValidandoDNI/Form1.Designer.cs
@@ -31,24 +31,23 @@
             this.txtDNI = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btValidar = new System.Windows.Forms.Button();
-            this.resultado = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // txtDNI
             // 
-            this.txtDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDNI.Location = new System.Drawing.Point(114, 104);
+            this.txtDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDNI.Location = new System.Drawing.Point(108, 110);
             this.txtDNI.Name = "txtDNI";
-            this.txtDNI.Size = new System.Drawing.Size(102, 29);
+            this.txtDNI.Size = new System.Drawing.Size(127, 35);
             this.txtDNI.TabIndex = 0;
             this.txtDNI.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI Light", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(61, 31);
+            this.label1.Location = new System.Drawing.Point(62, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(248, 40);
+            this.label1.Size = new System.Drawing.Size(209, 40);
             this.label1.TabIndex = 1;
             this.label1.Text = "Introduce el DNI: ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -56,7 +55,7 @@
             // btValidar
             // 
             this.btValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btValidar.Location = new System.Drawing.Point(126, 170);
+            this.btValidar.Location = new System.Drawing.Point(131, 194);
             this.btValidar.Name = "btValidar";
             this.btValidar.Size = new System.Drawing.Size(81, 29);
             this.btValidar.TabIndex = 2;
@@ -64,21 +63,12 @@
             this.btValidar.UseVisualStyleBackColor = true;
             this.btValidar.Click += new System.EventHandler(this.btValidar_Click);
             // 
-            // resultado
-            // 
-            this.resultado.Location = new System.Drawing.Point(68, 247);
-            this.resultado.Name = "resultado";
-            this.resultado.Size = new System.Drawing.Size(203, 20);
-            this.resultado.TabIndex = 3;
-            this.resultado.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected_1);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(346, 422);
-            this.Controls.Add(this.resultado);
             this.Controls.Add(this.btValidar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtDNI);
@@ -93,7 +83,6 @@
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btValidar;
-        private System.Windows.Forms.MaskedTextBox resultado;
         public System.Windows.Forms.MaskedTextBox txtDNI;
     }
 }
--- ValidandoDNI/Form1.cs
+++ ValidandoDNI/Form1.cs
@@ -15,7 +15,7 @@ namespace ValidandoDNI
         public Form1()
         {
             InitializeComponent();
-            // para que solo se puden introducir 8 números y una letra mayúscula
+            // para que solo se puden introducir 8 números y una letra mayúscula de primeras
             txtDNI.Mask = "00000000>L";
             txtDNI.AsciiOnly = true;
             txtDNI.ResetOnSpace = false;
@@ -36,17 +36,24 @@ namespace ValidandoDNI
         {
 
         }
-
+// DNI -> 8 Num  
         private void btValidar_Click(object sender, EventArgs e)
         {
-            string dni = txtDNI.Text.Replace(" ","");
-            if (dni.Length != 9)
+            string dni = txtDNI.Text;
+            int numero = int.Parse(dni.Substring(0, 8));
+            char letra = char.Parse(dni.Substring(8, 1));
+
+            //Las letras del DNI se corresponden con el resto de dividir el número del DNI entre 23 y ese número está asociado a una letra concreta
+            if (letra == ("TRWAGMYFPDXBNJZSQVHLCKE"[numero % 23])) //Este es el listado de las letras asociadas a cada número:
+
             {
-                resultado.Text = "DNI no válido";
-                return;
-
+                MessageBox.Show("DNI Válido");
             }
-
+            else
+            {
+                MessageBox.Show("DNI No Válido");
+            }
+            
 
         }
 
